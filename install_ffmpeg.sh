79c72c0054deb0dbf2e2e91e82bbe1aea398f3b8
Jai Luthra <me@jailuthra.in>
2021-07-02 14:29:04 +0530

    install_ffmpeg: Upgrade ffmpeg to v4.4

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -32,7 +32,7 @@ if [ ! -e "$HOME/ffmpeg/libavcodec/libavcodec.a" ]; then
 
   git clone https://github.com/livepeer/FFmpeg.git "$HOME/ffmpeg" || echo "FFmpeg dir already exists"
   cd "$HOME/ffmpeg"
-  git checkout b344789a31a2d806be702239d7614f7a9f51f941
+  git checkout 4f72e3c386c61e2952ccd46fa6cf1859d3873310 
   ./configure --prefix="$HOME/compiled" --enable-libx264 --enable-gnutls --enable-gpl --enable-static
   make
   make install
